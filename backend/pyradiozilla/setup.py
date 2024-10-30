39ac214a9e55d2a734d9d9864b25d32a9bd043bf
timurua <timurua@gmail.com>
2024-10-29 17:06:17 -0700

    Version update

--- backend/pyradiozilla/setup.py
+++ backend/pyradiozilla/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyradiozilla',
-    version='0.1.3',
+    version='0.1.4',
     description='Python library for importing data from external datasources',
     author='Radiozilla',
     author_email='timurua@gmail.com',

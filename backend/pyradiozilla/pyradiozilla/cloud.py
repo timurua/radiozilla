16a84c1e7efd435a7b2429a8bfc844884dc6b930
timur <timurua@gmail.com>
2024-11-05 17:45:30 -0800

    Firebase upload is implemented

--- backend/pyradiozilla/pyradiozilla/cloud.py
+++
@@ -1,9 +0,0 @@
-from pyradiozilla import storage
-from pyradiozilla import audio
-
-class Cloud:
-    def __init__(self, name: str, audio: audio.Audio) -> None:
-        self.audio = audio
-        
-    def save(self):
-        

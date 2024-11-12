50308b635160c10f620dd0886a1fc5c8e9d34f22
timurua <timurua@gmail.com>
2024-11-12 14:22:38 -0800

    Add radiozilla branding to the top of the page

--- web/player/src/components/TopNavbar.tsx
+++ web/player/src/components/TopNavbar.tsx
@@ -4,7 +4,8 @@ import { BsBell, BsSearch } from 'react-icons/bs';
 export default function TopNavbar() {
     return (
         <Navbar bg="dark" variant="dark" className="w-100 d-flex justify-content-between px-3" fixed="top">
-            <Navbar.Brand href="#">Podcasts</Navbar.Brand>
+            <Navbar.Brand href="#"><img src="/radiozilla.svg" alt="Radiozilla Logo" height="30" className="mr-2" /> Radio</Navbar.Brand>
+
             <div>
                 <BsBell size={20} className="mx-2 text-light" />
                 <BsSearch size={20} className="mx-2 text-light" />

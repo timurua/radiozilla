dc03eb05be8d3e77fc63840127958f02cace83a1
timur <timurua@gmail.com>
2025-01-16 18:50:25 -0800

    apiservice was switched to port 8000

--- backend/apiservice/genapi.sh
+++ backend/apiservice/genapi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o openapi.json http://localhost:8001/openapi.json && openapi-generator-cli generate \
+curl -o openapi.json http://localhost:8000/openapi.json && openapi-generator-cli generate \
   -i ./openapi.json \
   -g typescript-axios \
   -o ./ui/src/api
--- backend/run.apiservice.sh
+++ backend/run.apiservice.sh
@@ -5,4 +5,4 @@ export ENV_FILE=config/.env.local
 
 
 # Start the application
-uvicorn apiservice.backend.main:app --reload --host 0.0.0.0 --port 8001
+uvicorn apiservice.backend.main:app --reload --host 0.0.0.0 --port 8000

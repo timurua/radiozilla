b339e1a52a1682b5a5a908d96bb9a127bab15a5f
Timur Valiulin <timurua@gmail.com>
2025-05-05 21:56:40 -0700

    Added local running script

--- apps/pysrc/db/base.py
+++ apps/pysrc/db/base.py
@@ -9,5 +9,5 @@ class Base(DeclarativeBase):
 class TimestampModel(Base):
     __abstract__ = True
 
-    created_at = Column(DateTime(timezone=False), server_default=func.now())
-    updated_at = Column(DateTime(timezone=False), onupdate=func.now())
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
--- apps/run.dbjob.gcp.sh
+++ apps/run.dbjob.gcp.sh
@@ -5,4 +5,4 @@ export ENV_FILE=config/.env.local.gcp
 
 
 # Start the application
-uvicorn apiservice.backend.main:app --reload --host 0.0.0.0 --port 8000
+python -m dbjob.main

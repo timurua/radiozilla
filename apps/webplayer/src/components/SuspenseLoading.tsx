707416ec97fab0447fbc49269cb9eda25abeb7ee
Timur Valiulin <timurua@gmail.com>
2025-05-06 11:38:39 -0700

    removing separate webplayer app

--- apps/webplayer/src/components/SuspenseLoading.tsx
+++
@@ -1,7 +0,0 @@
-export function SuspenseLoading(){
-    return (
-        <div className="w-100 bg-dark text-white d-flex justify-content-center align-items-center">
-            Loading...
-        </div>
-    );
-}
